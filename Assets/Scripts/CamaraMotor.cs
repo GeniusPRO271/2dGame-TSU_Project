c7153d9bc9d32f4d57659000f642b98bc0e77a0b
GeniusPRO271 <benjamintoroj@gmail.com>
2022-06-05 14:05:33 +0700

    GameUpdate

---
+++ Assets/Scripts/CamaraMotor.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:93e6c44da270b6552900c9353b4d38100135ac4badedec6fe5db6b09d601a748
+size 1065
---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b09b6b7f993aec8fb7b4b1671bb78c0681a4f03c491f39ee30c7ac5e0a9139a
+size 1336
